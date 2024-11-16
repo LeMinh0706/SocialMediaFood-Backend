9cdd85948a2c48df7394840a3d94ee9d1073bbe3
LeMinh0706 <leminhken124356@gmail.com>
2024-11-16 23:43:19 +0700

    update err delete comment

---
+++ sql/schemas/20241116113745_report_post.sql
@@ -0,0 +1,33 @@
+-- +goose Up
+-- +goose StatementBegin
+CREATE TABLE "issue_post" (
+  "id" int PRIMARY KEY,
+  "name" varchar NOT NULL,
+  "is_deleted" bool
+);
+CREATE TABLE "report_post" (
+  "id" bigserial PRIMARY KEY,
+  "account_id" bigint NOT NULL,
+  "issue_id" int NOT NULL,
+  "post_id" bigint,
+  "created_at" timestamptz NOT NULL DEFAULT (now())
+);
+
+CREATE INDEX ON "report_post" ("post_id");
+
+CREATE INDEX ON "report_post" ("account_id");
+
+CREATE INDEX ON "report_post" ("issue_id");
+
+ALTER TABLE "report_post" ADD FOREIGN KEY ("account_id") REFERENCES "accounts" ("id");
+
+ALTER TABLE "report_post" ADD FOREIGN KEY ("issue_id") REFERENCES "issue_post" ("id");
+
+ALTER TABLE "report_post" ADD FOREIGN KEY ("post_id") REFERENCES "posts" ("id");
+-- +goose StatementEnd
+
+-- +goose Down
+-- +goose StatementBegin
+DROP TABLE IF EXISTS "issue_post" cascade;
+DROP TABLE IF EXISTS "report_post" cascade;
+-- +goose StatementEnd
